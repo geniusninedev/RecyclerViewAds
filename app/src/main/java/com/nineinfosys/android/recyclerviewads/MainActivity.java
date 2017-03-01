f2e459036c4921e5275a721de9ef0b028fd7ac62
geniusninedev <developer@nineinfosys.com>
2017-03-01 13:12:37 +0530

    Initial commit

---
+++ app/src/main/java/com/nineinfosys/android/recyclerviewads/MainActivity.java
@@ -0,0 +1,110 @@
+package com.nineinfosys.android.recyclerviewads;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.clockbyte.admobadapter.AdmobRecyclerAdapterWrapper;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.MobileAds;
+
+import java.util.ArrayList;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MainActivity extends AppCompatActivity {
+
+    RecyclerView rvMessages;
+    AdmobRecyclerAdapterWrapper adapterWrapper;
+    Timer updateAdsTimer;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        MobileAds.initialize(getApplicationContext(), getString(R.string.test_admob_app_id));
+
+        initRecyclerViewItems();
+        initUpdateAdsTimer();
+
+
+    }
+    private void initRecyclerViewItems() {
+        rvMessages = (RecyclerView) findViewById(R.id.rvMessages);
+        rvMessages.setLayoutManager(new LinearLayoutManager(this));
+
+        //creating your adapter, it could be a custom adapter as well
+        RecyclerExampleAdapter adapter  = new RecyclerExampleAdapter(this);
+
+        //your test devices' ids
+        String[] testDevicesIds = new String[]{getString(R.string.testDeviceID), AdRequest.DEVICE_ID_EMULATOR};
+        //when you'll be ready for release please use another ctor with admobReleaseUnitId instead.
+        adapterWrapper = new AdmobRecyclerAdapterWrapper(this, testDevicesIds);
+        //By default both types of ads are loaded by wrapper.
+        // To set which of them to show in the list you should use an appropriate ctor
+        //adapterWrapper = new AdmobRecyclerAdapterWrapper(this, testDevicesIds, EnumSet.of(EAdType.ADVANCED_INSTALLAPP));
+
+        //wrapping your adapter with a AdmobAdapterWrapper.
+        adapterWrapper.setAdapter(adapter);
+        //inject your custom layout and strategy of binding for installapp/content  ads
+        //here you should pass the extended NativeAdLayoutContext
+        //by default it has a value InstallAppAdLayoutContext.getDefault()
+        //adapterWrapper.setInstallAdsLayoutContext(...);
+        //by default it has a value ContentAdLayoutContext.getDefault()
+        //adapterWrapper.setContentAdsLayoutContext(...);
+
+        //Sets the max count of ad blocks per dataset, by default it equals to 3 (according to the Admob's policies and rules)
+        adapterWrapper.setLimitOfAds(50);
+
+        //Sets the number of your data items between ad blocks, by default it equals to 10.
+        //You should set it according to the Admob's policies and rules which says not to
+        //display more than one ad block at the visible part of the screen,
+        // so you should choose this parameter carefully and according to your item's height and screen resolution of a target devices
+        adapterWrapper.setNoOfDataBetweenAds(1);
+        adapterWrapper.setFirstAdIndex(1);
+
+        //if you use several view types in your source adapter then you have to set the biggest view type value with the following method
+        //adapterWrapper.setViewTypeBiggestSource(100);
+
+        rvMessages.setAdapter(adapterWrapper); // setting an AdmobRecyclerAdapterWrapper to a RecyclerView
+
+        //preparing the collection of data
+        final String sItem = "item #";
+        ArrayList<String> lst = new ArrayList<String>(100);
+        for(int i=1;i<=100;i++)
+            lst.add(sItem.concat(Integer.toString(i)));
+
+        //adding a collection of data to your adapter and rising the data set changed event
+        adapter.addAll(lst);
+        adapter.notifyDataSetChanged();
+    }
+
+    /*
+    * Could be omitted. It's only for updating an ad blocks in each 60 seconds without refreshing the list
+     */
+    private void initUpdateAdsTimer() {
+        updateAdsTimer = new Timer();
+        updateAdsTimer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        adapterWrapper.requestUpdateAd();
+                    }
+                });
+            }
+        }, 60*1000, 60*1000);
+    }
+
+    /*
+    * Seems to be a good practice to destroy all the resources you have used earlier :)
+     */
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if(updateAdsTimer!=null)
+            updateAdsTimer.cancel();
+        adapterWrapper.destroyAds();
+    }
+}
